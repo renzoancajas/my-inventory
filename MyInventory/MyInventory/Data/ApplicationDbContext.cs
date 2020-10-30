eddcf1e656998bf590f09a136cdf005436ac7096
Renz <11845354@benilde.edu.ph>
2020-10-30 18:23:25 +0800

    Initial commit test

---
+++ MyInventory/MyInventory/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyInventory.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
